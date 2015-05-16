08821adbc48becc4928361f4183518830f1120bf
Kristina Brown <kristina.frey.frey@gmail.com>
2015-05-16 10:44:20 -0600

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
                     length: {maximum: 244}, 
                     format: {with: VALID_EMAIL_REGEX },
                     uniqueness: {case_sensitive: false}
-  validates :password, length: {minimum: 6}
+  validates :password, length: {minimum: 6}, allow_blank: true
   has_secure_password
   
   def User.digest(string)
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,8 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'launchy'
+require 'minitest/pride'
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
