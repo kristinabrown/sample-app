9ecbd593e54250b06f451f0ceb73ffb5c1888130
Kristina Brown <kristina.frey.frey@gmail.com>
2015-05-17 13:54:35 -0600

    following working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   get 'password_resets/new'
-
   get 'password_resets/edit'
-
   get 'sessions/new'
-
   get 'users/new'
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
@@ -14,8 +11,14 @@ Rails.application.routes.draw do
   get 'login'  => 'sessions#new'
   post 'login'  => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
