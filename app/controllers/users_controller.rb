332712d8f0a83b77824a15d4248d5a5cc05f6e5d
Kristina Brown <kristina.frey.frey@gmail.com>
2015-05-16 21:21:27 -0600

    dont remember

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   end
   
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
   
   def create
